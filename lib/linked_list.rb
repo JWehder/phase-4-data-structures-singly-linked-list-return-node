dfb87f56d249953019808f1a4689b3ffd0e93f50
Jake Wehder <jw@email.comjake>
2023-04-19 11:40:07 -0500

    creating a movies reviewed category for the user to view

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,15 @@ class LinkedList
 
   def nth_from_end(n)
     # your code here
+    # given a number n, how far is n from the end of the list
+    # there needs to be a measurement of the list with length
+    # conditional to immediately get rid of n if greater than length
+    # have a counter that traverses through the list till it reaches the end
+    if n > self.length
+      nil
+    end
+    while 
+
   end
 
 end
